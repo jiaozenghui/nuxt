8c7ad0074d9f1faa831fe827fcb959183bcbe0ae
jiaozh1 <jiaozh1@lenovo.com>
2022-07-07 17:49:46 +0800

    blog add articles

--- frontend/pages/new_article.vue
+++ frontend/pages/new_article.vue
@@ -1,8 +1,79 @@
 <template>
   <div>
-    <no-ssr>
-      <ueditor :id="ue2" :config="config2" :defaultMsg="defaultMsg"></ueditor>
-    </no-ssr>
+      <form class="form-horizontal">
+        <div class="form-group-right clearfix">
+          <label for="title">
+            标题
+          </label>
+          <div class="form-group-right">
+            <input required
+              id="title"
+              title="title"
+              class="form-control verify-input"
+              type="text"
+              ng-model="article.title"
+              ng-maxlength="255"
+              placeholder="请输入文章标题"/>
+          </div>
+        </div>
+        <div class="form-group-right clearfix">
+          <label for="title">
+            分类
+          </label>
+          <div class="form-group-right">
+            <label class="radio-inline" ng-repeat= "category in categories">
+              <input type="radio" ng-value="category._id" ng-checked="article.category == category._id" name="category" ng-model="article.category" />{{category.name}}
+            </label>
+          </div>
+        </div>
+        <div class="form-group-right clearfix">
+          <label for="title">
+            配图
+          </label>
+          <div class="form-group-right">
+            <label class="radio-inline" >
+              <input type="file" ng-model="article_image" name="f" id="fileupload">
+            </label>
+          </div>
+        </div>
+        
+        <div class="form-group-right clearfix">
+          <label for="abstract">
+            摘要
+          </label>
+          <div class="form-group-right">
+            <textarea
+              id="abstract"
+              name="abstract"
+              class="form-control"
+              rows="4"
+              ng-maxlength="255"
+              type="abstract"
+              ng-model="article.abstract"
+              placeholder="请输入文章摘要">
+            </textarea>
+          </div>
+        </div>
+
+        <div class="form-group-right clearfix">
+          <label for="editor">
+            文章内容
+          </label>
+          <div class="form-group-right">
+            <div class="form-group-right">
+              <no-ssr>
+                <ueditor :id="ue2" :config="config2" :defaultMsg="defaultMsg"></ueditor>
+              </no-ssr>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-10 col-sm-10">
+            <button class="btn btn-primary" ng-click="addArticle()" type="submit">{{btnTitle}}</button>
+          </div>
+        </div>
+      </form>
+
   </div>
 </template>
 
